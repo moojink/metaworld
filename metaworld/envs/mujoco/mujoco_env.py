6fc39cf58c379393fec47bcd001d7f32ed73ff99
Moo Jin Kim <moojink@stanford.edu>
2021-08-05 02:19:40 -0700

    Add view_1_straight

--- metaworld/envs/mujoco/mujoco_env.py
+++ metaworld/envs/mujoco/mujoco_env.py
@@ -123,9 +123,9 @@ class MujocoEnv(gym.Env, abc.ABC):
         # If `camera_name` is "configured_view", we render with whatever view that
         # self._view is configured to (set during initialization of the environment).
         assert_string = ("camera_name should be one of ",
-                "corner3, corner, corner2, topview, gripperPOV, behindGripper, view_1, view_1_alt view_3", "configured_view")
+                "corner3, corner, corner2, topview, gripperPOV, behindGripper, view_1, view_1_straight, view_1_alt, view_3", "configured_view")
         assert camera_name in {"corner3", "corner", "corner2", 
-            "topview", "gripperPOV", "behindGripper", "view_1", "view_1_alt", "view_3", "configured_view"}, assert_string
+            "topview", "gripperPOV", "behindGripper", "view_1", "view_1_straight", "view_1_alt", "view_3", "configured_view"}, assert_string
         if not offscreen:
             self._get_viewer('human').render()
         else:
@@ -138,6 +138,8 @@ class MujocoEnv(gym.Env, abc.ABC):
                             "view. Defaulting to an arbitrary view (e.g., camera_name='view_3').")
                 if self._view == 1:
                     camera_name = "view_1"
+                elif self._view == "1_straight":
+                    camera_name = "view_1_straight"
                 elif self._view == "1_alt":
                     camera_name = "view_1_alt"
                 else:
@@ -181,7 +183,7 @@ class MujocoEnv(gym.Env, abc.ABC):
         return self.data.get_body_xpos(body_name)
 
     def set_camera_view(self, view):
-        assert view == 1 or view == 3 or view == "1_alt"
+        assert view == 1 or view == 3 or view == "1_alt" or view == "1_straight"
         self._view = view
 
     def set_render_img_size(self, render_img_size):
