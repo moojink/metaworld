9785772ed0c40badeb3b2a9a1b0ac76823769527
Moo Jin Kim <moojink@stanford.edu>
2021-09-18 14:40:12 -0700

    Set up train/test for door open

--- metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_door_v2.py
+++ metaworld/envs/mujoco/sawyer_xyz/v2/sawyer_door_v2.py
@@ -8,28 +8,50 @@ from metaworld.envs.mujoco.sawyer_xyz.sawyer_xyz_env import SawyerXYZEnv, _asser
 
 
 class SawyerDoorEnvV2(SawyerXYZEnv):
-    def __init__(self):
+    def __init__(self, view, train, random_init_obj_pos):
 
-        hand_low = (-0.5, 0.40, 0.05)
+        hand_low = (-0.5, 0.20, 0.05)
         hand_high = (0.5, 1, 0.5)
-        obj_low = (0., 0.85, 0.15)
-        obj_high = (0.1, 0.95, 0.15)
-        goal_low = (-.3, 0.4, 0.1499)
-        goal_high = (-.2, 0.5, 0.1501)
+
+        self.train = train
+        self.train_positions = dict(
+            obj_low = (-0.2, 0.85, 0.15),
+            obj_high =  (-0.1, 0.85, 0.15),
+            # goal_low = (-.3, 0.4, 0.1499),
+            # goal_high = (-.2, 0.5, 0.1501),
+        )
+        self.test_positions = dict(
+            obj_low = (-0.3, 0.8, 0.15),
+            obj_high =  (-0.1, 0.95, 0.15),
+            # goal_low = (-.3, 0.4, 0.1499),
+            # goal_high = (-.2, 0.5, 0.1501),
+        )
+        if self.train:
+            obj_low = self.train_positions['obj_low']
+            obj_high = self.train_positions['obj_high']
+            # goal_low = self.train_positions['goal_low']
+            # goal_high = self.train_positions['goal_high']
+        else:
+            obj_low = self.test_positions['obj_low']
+            obj_high = self.test_positions['obj_high']
+            # goal_low = self.test_positions['goal_low']
+            # goal_high = self.test_positions['goal_high']
 
         super().__init__(
             self.model_name,
+            view=view,
             hand_low=hand_low,
             hand_high=hand_high,
+            random_init_obj_pos=random_init_obj_pos,
         )
 
         self.init_config = {
             'obj_init_angle': np.array([0.3, ]),
             'obj_init_pos': np.array([0.1, 0.95, 0.15]),
-            'hand_init_pos': np.array([0, 0.6, 0.2]),
+            'hand_init_pos': np.array([0.1, 0.4, 0.2]),
         }
 
-        self.goal = np.array([-0.2, 0.7, 0.15])
+        # self.goal = np.array([-0.2, 0.7, 0.15])
         self.obj_init_pos = self.init_config['obj_init_pos']
         self.obj_init_angle = self.init_config['obj_init_angle']
         self.hand_init_pos = self.init_config['hand_init_pos']
@@ -41,7 +63,7 @@ class SawyerDoorEnvV2(SawyerXYZEnv):
             np.array(obj_low),
             np.array(obj_high),
         )
-        self.goal_space = Box(np.array(goal_low), np.array(goal_high))
+        # self.goal_space = Box(np.array(goal_low), np.array(goal_high))
 
     @property
     def model_name(self):
@@ -87,13 +109,21 @@ class SawyerDoorEnvV2(SawyerXYZEnv):
         qvel[self.door_angle_idx] = 0
         self.set_state(qpos.flatten(), qvel.flatten())
 
-    def reset_model(self):
+    def reset_model(self, seed=None):
         self._reset_hand()
 
-        self.objHeight = self.data.get_geom_xpos('handle')[2]
+        if seed is not None:
+            np.random.seed(seed=seed) # this ensures that every time we reset, we get the same initial obj positions
+        # np.random.seed(seed=0) # this ensures that every time we reset, we get the same initial obj positions
 
-        self.obj_init_pos = self._get_state_rand_vec() if self.random_init \
-            else self.init_config['obj_init_pos']
+        self.objHeight = self.data.get_geom_xpos('handle')[2]
+        self.obj_init_pos = self.init_config['obj_init_pos']
+
+        if self.random_init:
+            self.obj_init_pos = self._get_state_rand_vec()
+            while self.should_resample_obj_pos(self.obj_init_pos):
+                self.obj_init_pos = self._get_state_rand_vec()
+
         self._target_pos = self.obj_init_pos + np.array([-0.3, -0.45, 0.])
 
         self.sim.model.body_pos[self.model.body_name2id('door')] = self.obj_init_pos
@@ -103,6 +133,20 @@ class SawyerDoorEnvV2(SawyerXYZEnv):
         self.target_reward = 1000*self.maxPullDist + 1000*2
 
         return self._get_obs()
+
+    def should_resample_obj_pos(self, obj_pos):
+        """Returns True when the initial position of the object
+        overlaps with the distribution of initial positions used during training.
+        This is so that during test time we sample positions outside of the training
+        distribution."""
+        if self.train:
+            return False # only possibly resample during testing
+        obj_x, obj_y = obj_pos[0], obj_pos[1]
+        obj_low_x, obj_low_y = self.train_positions['obj_low'][0], self.train_positions['obj_low'][1]
+        obj_high_x, obj_high_y = self.train_positions['obj_high'][0], self.train_positions['obj_high'][1]
+        # Only check x and y because z is the same during train and test.
+        return obj_low_x <= obj_x and obj_x <= obj_high_x and obj_low_y <= obj_y and obj_y <= obj_high_y
+
 
     @staticmethod
     def _reward_grab_effort(actions):
